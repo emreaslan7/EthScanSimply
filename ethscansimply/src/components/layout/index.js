14cec33adb339e4da3216e5a2e894f4ba9733119
Emre Aslan <emreaslan7@icloud.com>
2023-02-26 16:58:35 +0300

    first-commit

---
+++ ethscansimply/src/components/layout/index.js
@@ -0,0 +1,12 @@
+// import Navbar from '@/sections/navbar'
+// import Footer from '@/sections/footer'
+
+export default function Layout({ children }) {
+    return (
+        <>
+            {/* <Navbar /> */}
+            <main>{children}</main>
+            {/* <Footer /> */}
+        </>
+    )
+}
