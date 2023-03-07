8f8273723d7c2d5397c7d852d71a49e586698e57
Emre Aslan <emreaslan7@icloud.com>
2023-03-07 19:06:53 +0300

    blockchain page update

--- ethscansimply/src/pages/blockchain/index.js
+++ ethscansimply/src/pages/blockchain/index.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Image } from '@chakra-ui/react';
-import { Blockchainquery } from "../../ethers/blockchainquery";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
+import React, { useState, useEffect } from "react";
+import { Box, Container, Flex, Image, Collapse, Button} from "@chakra-ui/react";
+import { Blockchainquery } from "../../ethers/blockchainquery";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
 
 const blockchain = new Blockchainquery();
 
+
+
 function Blockchain() {
   const [blockNumber, setBlockNumber] = useState(null);
   const [block, setBlock] = useState(null);
+
+  const { isOpen, onToggle } = useDisclosure();
 
   useEffect(() => {
     async function fetchBlockchainData() {
@@ -17,54 +22,102 @@ function Blockchain() {
       const latestBlock = await blockchain.getBlock(latestBlockNumber);
       setBlockNumber(latestBlockNumber);
       setBlock(latestBlock);
-
     }
     fetchBlockchainData();
   }, []);
 
   return (
-    <div>
-      <Box display={'flex'} alignItems='center' justifyContent={'center'}>
-      <Box w={'100px'} h={'100px'}>
-        <Image src='/assets/block.png' objectFit='cover'/>
-      </Box>
-      <Box w={'100px'} h={'100px'} display='flex' alignItems='center' justifyContent={'center'}>
-        <Image src='/assets/chain.png' objectFit='cover'/>
-      </Box>
-      </Box>
-
-      <Tabs colorScheme='teal' align='left'>
-      <TabList mb='1em'>
-      <Tab><Box w={'100px'} h={'100px'}>
-        <Image src='/assets/block.png' objectFit='cover'/>
-      </Box></Tab>
-      <Box w={'100px'} h={'100px'} display='flex' alignItems='center' justifyContent={'center'}>
-        <Image src='/assets/chain.png' objectFit='cover'/>
-      </Box>
-      <Tab>      <Box w={'100px'} h={'100px'}>
-        <Image src='/assets/block.png' objectFit='cover'/>
-      </Box></Tab>
-      <Box w={'100px'} h={'100px'} display='flex' alignItems='center' justifyContent={'center'}>
-        <Image src='/assets/chain.png' objectFit='cover'/>
-      </Box>
-      <Tab>      <Box w={'100px'} h={'100px'}>
-        <Image src='/assets/block.png' objectFit='cover'/>
-      </Box></Tab>
-      </TabList>
-      <TabPanels>
-      <TabPanel>
-      <p>Block 1</p>
-      </TabPanel>
-      <TabPanel>
-      <p>Block2</p>
-      </TabPanel>
-      <TabPanel>
-      <p>Block3</p>
-      </TabPanel>
-      </TabPanels>
-      </Tabs>
+    <Container maxW='container.xl' bg='green.400'>
+      <Flex direction={{ base: "column", lg: "row" }}>
 
       
+      <Tabs colorScheme="whiteAlpha" align="center" defaultIndex={2} w={{base:'100%', lg:'50%'}}>
+        <TabList
+          mb="1em"
+          style={{ backgroundColor: "transparent", borderBottom: "none" }}
+        >
+          <Tab
+            opacity={0.72}
+            transition="all 0.5s ease-in-out"
+            _selected={{ transform: "scale(1.4)", opacity: "1" }}
+            _focus={{ bg: "transparent", boxShadow: "none" }}
+          >
+            <Box w={"100px"} h={"100px"}>
+              <Image src="/assets/block.png" objectFit="cover" />
+            </Box>
+          </Tab>
+          <Box
+            w={"100px"}
+            h={"100px"}
+            display="flex"
+            alignItems="center"
+            justifyContent={"center"}
+          >
+            <Box w={"100px"}>
+              <Image src="/assets/chain.png" objectFit="cover" />
+            </Box>
+          </Box>
+          <Tab
+            opacity={0.72}
+            transition="all 0.5s ease-in-out"
+            _selected={{ transform: "scale(1.4)", opacity: "1" }}
+            _focus={{ bg: "transparent", boxShadow: "none" }}
+          >
+            <Box w={"100px"} h={"100px"}>
+              <Image src="/assets/block.png" objectFit="cover" />
+            </Box>
+          </Tab>
+          <Box
+            w={"100px"}
+            h={"100px"}
+            display="flex"
+            alignItems="center"
+            justifyContent={"center"}
+          >
+            <Box w={"100px"}>
+              <Image src="/assets/chain.png" objectFit="cover" />
+            </Box>
+          </Box>
+          <Tab
+            opacity={0.72}
+            transition="all 0.5s ease-in-out"
+            _selected={{ transform: "scale(1.4)", opacity: "1" }}
+            _focus={{ bg: "transparent", boxShadow: "none" }}
+          >
+            <Box w={"100px"} h={"100px"}>
+              <Image src="/assets/block.png" objectFit="cover" />
+            </Box>
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>Block 1</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Block2</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Block3</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+      <Box w={{base:'100%', lg:'50%'}}>
+      <Button onClick={onToggle}>Click Me</Button>
+      <Collapse in={isOpen} animateOpacity>
+        <Box
+          p='40px'
+          color='white'
+          mt='4'
+          bg='teal.500'
+          rounded='md'
+          shadow='md'
+        >
+           sEAŞDFLŞASDMFLŞAMFLŞADMALŞMDŞLAMDFLŞAMDF
+        </Box>
+      </Collapse>
+      </Box>
+
       {/* <h1>Son blok numarası: {blockNumber}</h1>
       {block && (
         <div>
@@ -78,7 +131,8 @@ function Blockchain() {
           </ul>
         </div>
       )} */}
-    </div>
-  )
+      </Flex>
+    </Container>
+  );
 }
 export default Blockchain;
