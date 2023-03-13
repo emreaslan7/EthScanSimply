3d2dfaa325d51d9536535532ada66b8451d02c02
Emre Aslan <emreaslan7@icloud.com>
2023-03-13 20:55:54 +0300

    tx query update

--- ethscansimply/src/ethers/blockchainquery/index.js
+++ ethscansimply/src/ethers/blockchainquery/index.js
@@ -1,9 +1,11 @@
-import { ethers, toBigInt} from "ethers";
+import { ContractFactory, ethers, toBigInt } from "ethers";
 import { type } from "os";
 
 export class Blockchainquery {
   constructor() {
-    this.provider = new ethers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
+    this.provider = new ethers.JsonRpcProvider(
+      `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+    );
   }
 
   async getBlockNumber() {
@@ -13,7 +15,6 @@ export class Blockchainquery {
 
   async getBlock(blockNumber) {
     const block = await this.provider.getBlock(Number(blockNumber));
-    console.log(block);
     return block;
   }
 
@@ -21,7 +22,44 @@ export class Blockchainquery {
     if (!address) {
       return "";
     }
-    return `${address.substring(0, 6)}....${address.substring(address.length -4)}`;
+    return `${address.substring(0, 6)}....${address.substring(
+      address.length - 4
+    )}`;
   }
 
+
+  async getTransaction(txHash) {
+    // getTransactionReceipt() kullanarak logları al
+    const contractAddress = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+    const contractABIerc20=[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
+    const contract = new ethers.Contract(contractAddress, contractABIerc20, this.provider);
+
+    console.log(contract);
+    this.provider
+      .getTransactionReceipt(txHash)
+      .then((receipt) => {
+        
+        // Transaction'da log var mı yok mu kontrol et
+        if (receipt.logs.length > 0) {
+          // Her bir log için, olayın adı ve parametrelerini yazdır
+          receipt.logs.forEach((log) => {
+            const event = contract.interface.parseLog(log);
+     
+            // console.log(event);
+            if(event != null){
+              console.log("Event Name:", event.name);
+              console.log("Event from:", event.args[0]);
+              console.log("Event to:", event.args[1]);
+              console.log("Event amount:", event.args[2]);
+            }
+
+          });
+        } else {
+          console.log("Transaction does not contain any logs.");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
--- ethscansimply/src/pages/txdetails/[[...txhash]]/index.js
+++ ethscansimply/src/pages/txdetails/[[...txhash]]/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Blockchainquery } from '../../../ethers/blockchainquery';
 
 function TxHash({ txhash }) {
+  const bq = new Blockchainquery();
+  const txHash = '0x2346878731ba7aa0882367d5434c7fef53a9e0a63209dde9d8416f2ca81ca8c3'; // işlem kimliği burada yer alacak
+  useEffect(() => {
+    async function fetchTxData(){
+      const tx = await bq.getTransaction(txHash);
+      console.log(tx);
+    }
+    fetchTxData();
+  })
 
   return (
-    <div>TxHash: {txhash}</div>
+    <div>
+
+      TxHash: {txhash}
+
+
+    </div>
   )
 }
 
