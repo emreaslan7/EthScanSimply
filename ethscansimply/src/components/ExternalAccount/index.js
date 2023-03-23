78a0412f86c5311526d7b28338dc4d09d223e107
Emre Aslan <emreaslan7@icloud.com>
2023-03-24 01:17:44 +0300

    update

--- ethscansimply/src/components/ExternalAccount/index.js
+++ ethscansimply/src/components/ExternalAccount/index.js
@@ -1,25 +1,35 @@
-import { Box, Image, Flex} from '@chakra-ui/react'
+import { Box, Image, Text} from '@chakra-ui/react'
 import React from 'react'
 
 function ExternalAccount({Address}) {
   return (
 
-        <Box position="relative" maxHeight="100vh">
-          {/* account-head.png - Üst kenarın ortasında */}
-          <Box position="absolute" top="0" left="50%" transform="translateX(-50%)">
-            <Image src="/assets/account-head.png" />
-          </Box>
-    
-          {/* account-left-circle.png - Ekranın sağına bitişik */}
-          <Box position="absolute" top="50%" right="0" transform="translateY(0)">
-            <Image src="/assets/account-left-circle.png" w="350px" h="auto"/>
-          </Box>
-    
-          {/* account-right-circle.png - Ekranın soluna bitişik */}
-          <Box position="absolute" top="50%" left="0" transform="translateY(0%)">
-            <Image src="/assets/account-right-circle.png" w="350px" h="auto"/>
-          </Box>
-        </Box>
+  <Box position="relative" maxHeight="100vh">
+    {/* account-head.png - Üst kenarın ortasında */}
+    <Box position="absolute" top="0" left="50%" transform="translateX(-50%)">
+      <Image src="/assets/account-head.png" minW={'300px'}/>
+      <Box position="absolute" top="25%" left="50%" transform="translate(-50%, -50%)">
+        <Text color={'black'}>{Address.addr}</Text>
+      </Box>
+    </Box>
+
+    {/* account-right-circle.png - Ekranın soluna bitişik */}
+    <Box position="fixed" bottom="4" left="0" >
+      <Image src="/assets/account-right-circle.png" h={{base:'60vh', md:'85vh', lg:'95vh'}} w='auto'/>
+      <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)">
+        <Text color={'black'}>{Address.balance}</Text>
+      </Box>
+    </Box>
+
+    {/* account-left-circle.png - Ekranın sağına bitişik */}
+    <Box position="fixed" top={{base:'32',md:"10"}} right="0" >
+      <Image src="/assets/account-left-circle.png"  h={{base:'60vh', md:'85vh', lg:'95vh'}} w='auto'/>
+      <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)">
+        <Text color={'black'}>{Address.nonce}</Text>
+      </Box>
+    </Box>
+  </Box>
+
   
   )
 }
